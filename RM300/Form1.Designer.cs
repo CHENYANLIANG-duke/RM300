11f3b30097a2600fc27656b3263cb3fc6dddc9bb
CHENYANLIANG-duke <46186328+CHENYANLIANG-duke@users.noreply.github.com>
2019-01-02 18:06:51 +0800

    加入專案檔案。

---
+++ RM300/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace RM300
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CmbSerialPort = new System.Windows.Forms.ComboBox();
+            this.btnConn = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(23, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ComPort :";
+            // 
+            // CmbSerialPort
+            // 
+            this.CmbSerialPort.FormattingEnabled = true;
+            this.CmbSerialPort.Location = new System.Drawing.Point(100, 15);
+            this.CmbSerialPort.Name = "CmbSerialPort";
+            this.CmbSerialPort.Size = new System.Drawing.Size(148, 20);
+            this.CmbSerialPort.TabIndex = 1;
+            // 
+            // btnConn
+            // 
+            this.btnConn.Location = new System.Drawing.Point(449, 12);
+            this.btnConn.Name = "btnConn";
+            this.btnConn.Size = new System.Drawing.Size(75, 46);
+            this.btnConn.TabIndex = 2;
+            this.btnConn.Text = "Connect";
+            this.btnConn.UseVisualStyleBackColor = true;
+            this.btnConn.Click += new System.EventHandler(this.btnConn_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(449, 142);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 46);
+            this.btnRead.TabIndex = 3;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(449, 203);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 46);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(404, 198);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnConn);
+            this.Controls.Add(this.CmbSerialPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CmbSerialPort;
+        private System.Windows.Forms.Button btnConn;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ RM300/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using ThingMagic;
+
+
+namespace RM300
+{
+    public partial class Form1 : Form
+    {
+        public Reader objReader = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                CmbSerialPort.Items.Add(port);
+            }
+        }
+
+        
+
+        public void SerialPortRead()
+        {
+            SerialPort SerialPort1 = new SerialPort();
+
+            SerialPort1.BaudRate = 115200;
+            SerialPort1.PortName = CmbSerialPort.SelectedItem.ToString();
+
+            try
+            {
+                SerialPort1.Open();
+                MessageBox.Show("Connect Successful!");
+                MessageBox.Show(SerialPort1.PortName + "," + SerialPort1.Parity +","+SerialPort1.BaudRate + ","+ SerialPort1.StopBits);
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+       
+        private void btnConn_Click(object sender, EventArgs e)
+        {
+            SerialPortRead();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int TotalTagCount = 0;
+                btnRead.Enabled = false;
+                TagReadData[] tagID = objReader.Read(int.Parse(tbxReadTimeout.Text));
+                arr = new ReadTags[tagID.Length];
+                for (int i = 0; i < tagID.Length; i++)
+                {
+                    arr[i] = new ReadTags(tagID[i]);
+                    TotalTagCount = TotalTagCount + tagID[i].ReadCount;
+                }
+                dataGrid1.DataSource = arr;
+                generatedatagrid();
+                lblTotalTagCount.Text = TotalTagCount.ToString();
+                lblUniqueTagCount.Text = tagID.Length.ToString();
+                btnRead.Enabled = true;
+                btnRead.Focus();
+
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+                objReader.Destroy();
+                objReader = null;
+                btnRead.Enabled = false;
+                btnConnect.Text = "Connect";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Reader Message");
+                btnRead.Enabled = true;
+            }
+        }
+
+        public class ReadTags
+        {
+            private string epc;
+            private string timestamp;
+            private string readcount;
+            public string rssi;
+
+            public ReadTags()
+            {
+            }
+            public ReadTags(TagReadData adddata)
+            {
+                epc = adddata.EpcString;
+                timestamp = adddata.Time.ToString();
+                readcount = adddata.ReadCount.ToString();
+                rssi = adddata.Rssi.ToString();
+            }
+            public string EPC
+            {
+                get { return epc; }
+            }
+            public string Timestamp
+            {
+                get { return timestamp; }
+            }
+            public string ReadCount
+            {
+                get { return readcount; }
+            }
+            public string Rssi
+            {
+                get { return rssi; }
+            }
+
+        }
+    }
+}
